d2b59ec61b4a038ded6b8b3cd748b09ba8a7d294
Fábio <fabiodcs@gmail.com>
2020-07-12 08:36:52 -0300

    Fim da lição 49

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,10 +11,8 @@ using API.Dtos;
 using AutoMapper;
 
 namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+{   
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
