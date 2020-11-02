db2178ad44b09a5a059031dbb5d93f829051589d
Fábio <fabiodcs@gmail.com>
2020-11-01 22:43:45 -0300

    Fim da lição 63, adicionando paginação.

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -17,9 +17,22 @@ namespace Core.Specifications
         public List<Expression<Func<T, object>>> Includes { get; } =
             new List<Expression<Func<T, object>>>();
 
-        public Expression<Func<T, object>> OrderBy {get; private set;}
+        public Expression<Func<T, object>> OrderBy { get; private set; }
 
-        public Expression<Func<T, object>> OrderByDescending  {get; private set;}
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public BaseSpecification(int take, int skip, bool isPagingEnabled)
+        {
+            this.Take = take;
+            this.Skip = skip;
+            this.isPagingEnabled = isPagingEnabled;
+
+        }
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool isPagingEnabled { get; private set; }
 
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
@@ -34,6 +47,13 @@ namespace Core.Specifications
         protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
         {
             OrderByDescending = orderByDescExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            isPagingEnabled = true;
         }
     }
 }
