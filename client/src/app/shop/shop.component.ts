78f50aaf9590adc05677a94707b7247f7706a483
Fábio <fabiodcs@gmail.com>
2020-11-14 16:26:57 -0300

    Fim da lição 93

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
 import { IProduct } from '../shared/models/product';
+import { IProductType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,10 +12,19 @@ import { ShopService } from './shop.service';
 export class ShopComponent implements OnInit {
 
   products: IProduct[];
+  brands: IBrand[];
+  productTypes: IProductType[];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getProductTypes();
+  }
+
+  // tslint:disable-next-line: typedef
+  getProducts() {
     this.shopService.getProducts().subscribe(response => {
       this.products = response.data;
     }, error => {
@@ -21,4 +32,21 @@ export class ShopComponent implements OnInit {
     });
   }
 
+  // tslint:disable-next-line: typedef
+  getBrands() {
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  getProductTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      this.productTypes = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
+import { IBrand } from '../shared/models/brands';
+import { IProductType } from '../shared/models/productType';
 
 @Injectable({providedIn: 'root'})
 
@@ -12,5 +14,15 @@ export class ShopService {
    // tslint:disable-next-line: typedef
    getProducts() {
     return this.http.get<IPagination>(this.baseUrl + 'products?PageSize=50');
+  }
+
+  // tslint:disable-next-line: typedef
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+   // tslint:disable-next-line: typedef
+   getTypes() {
+    return this.http.get<IProductType[]>(this.baseUrl + 'products/types');
   }
 }
