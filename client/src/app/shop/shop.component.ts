d6b039f899264c8d8a370bdcb72ba43c0b052b31
Fábio <fabiodcs@gmail.com>
2020-11-14 16:45:47 -0300

    Fim da lição 94

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -14,6 +14,8 @@ export class ShopComponent implements OnInit {
   products: IProduct[];
   brands: IBrand[];
   productTypes: IProductType[];
+  brandIdSelected: number;
+  typeIdSelected: number;
 
   constructor(private shopService: ShopService) { }
 
@@ -25,7 +27,7 @@ export class ShopComponent implements OnInit {
 
   // tslint:disable-next-line: typedef
   getProducts() {
-    this.shopService.getProducts().subscribe(response => {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(response => {
       this.products = response.data;
     }, error => {
       console.log(error);
@@ -35,7 +37,8 @@ export class ShopComponent implements OnInit {
   // tslint:disable-next-line: typedef
   getBrands() {
     this.shopService.getBrands().subscribe(response => {
-      this.brands = response;
+      // The three dots is a "spread operator" that joins 2 arrays
+      this.brands = [{id: 0, name: 'All'}, ...response];
     }, error => {
       console.log(error);
     });
@@ -44,9 +47,24 @@ export class ShopComponent implements OnInit {
   // tslint:disable-next-line: typedef
   getProductTypes() {
     this.shopService.getTypes().subscribe(response => {
-      this.productTypes = response;
+      // The three dots is a "spread operator" that joins 2 arrays
+      this.productTypes = [{id: 0, name: 'All'}, ...response];
     }, error => {
       console.log(error);
     });
   }
+
+  // tslint:disable-next-line: typedef
+  onBrandSelected(brandId: number)
+  {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+   // tslint:disable-next-line: typedef
+   onProductTypeSelected(typeId: number)
+   {
+     this.typeIdSelected = typeId;
+     this.getProducts();
+   }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
 import { IBrand } from '../shared/models/brands';
 import { IProductType } from '../shared/models/productType';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 
@@ -12,8 +13,25 @@ export class ShopService {
   constructor(private http: HttpClient) { }
 
    // tslint:disable-next-line: typedef
-   getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products?PageSize=50');
+   getProducts(brand?: number, typeId?: number) {
+    let params = new HttpParams();
+
+    if (brand) {
+      params = params.append('brandId', brand.toString());
+    }
+
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    // Projects the "response" object and return its body. This is necessary because the function returns first a observable
+    // and we need the body of this observable
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
   }
 
   // tslint:disable-next-line: typedef
