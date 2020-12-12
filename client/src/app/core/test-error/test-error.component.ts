0490b49f336c653db88203ccfbefffe696cde0a7
Fábio <fabiodcs@gmail.com>
2020-12-12 09:02:05 -0300

    Fazendo aula 116. Não conlcuído ver "TODO" em "test-error.components.ts".

--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-test-error',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-error.component.scss']
 })
 export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  // TODO: Revisar porque a mensagem de erro não está sendo retornada como esperado
+  // tslint:disable-next-line: typedef
+  get404Error()
+  {
+    this.http.get(this.baseUrl + 'products/42').subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  get500Error()
+  {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  get400Error()
+  {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  get400ValidationError()
+  {
+    this.http.get(this.baseUrl + 'products/fortyTwo').subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      console.log(error);
+    });
+  }
 }
