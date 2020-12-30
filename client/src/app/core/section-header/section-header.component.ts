94c610d87257ee8344a88e0ae6efb8020241eb8a
Fábio <fabiodcs@gmail.com>
2020-12-30 08:09:02 -0300

    Fim da lição 127

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent implements OnInit {
+  // By convention, all observables has a dollar sign on the end of it's name
+  breadcrumb$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private breadCrumbService: BreadcrumbService) {  }
 
   ngOnInit(): void {
+    // Populating the observable to be used in the template
+    this.breadcrumb$ = this.breadCrumbService.breadcrumbs$;
   }
 
 }
