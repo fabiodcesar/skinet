8136e6ea59cd33a68ec679d170c53bba58170b4c
Fábio <fabiodcs@gmail.com>
2021-01-07 11:27:31 -0300

    Fim da lição 168

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using API.Dtos;
+using API.Errors;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto login)
+        {
+            var user = await _userManager.FindByEmailAsync(login.Email);
+
+            if (user == null)
+                return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, false);
+
+            if (!result.Succeeded)
+                return Unauthorized(new ApiResponse(401));
+
+            return new UserDto()
+            {
+                Email = user.Email,
+                DisplayName = user.DisplayName,
+                Token = "This will be a token"
+            };
+        }
+    }
+}
