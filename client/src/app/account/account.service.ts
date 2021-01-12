aead90225729a2036c76ca7e861a0267956eadac
Fábio <fabiodcs@gmail.com>
2021-01-12 08:38:57 -0300

    Fim da lição 187

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<IUser>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  // tslint:disable-next-line: typedef
+  login(values: any)
+  {
+    return this.http.post(this.baseUrl + 'account/login', values).pipe(
+      map((user: IUser) => {
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  register(values: any) {
+    return this.http.post(this.baseUrl + 'register', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  logout(){
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  // tslint:disable-next-line: typedef
+  checkEmailExists(email: string){
+    return this.http.get(this.baseUrl + '/account/emailExists?email=' + email);
+  }
 }
