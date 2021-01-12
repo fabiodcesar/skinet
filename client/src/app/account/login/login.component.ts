7a9cd73808140d58db440a3e0dbe1a175697408f
Fábio <fabiodcs@gmail.com>
2021-01-12 12:13:12 -0300

    Fim da lição 190

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  // tslint:disable-next-line: typedef
+  createLoginForm()
+  {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  onSubmit()
+  {
+    console.log(this.loginForm.value);
   }
 
 }
