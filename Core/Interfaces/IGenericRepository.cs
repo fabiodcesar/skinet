9b8db7c455128d70a6ae097612f330f8df986fe0
Fábio <fabiodcs@gmail.com>
2021-02-08 07:22:41 -0300

    Fim da lição 218

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -5,6 +5,9 @@ using Core.Specifications;
 
 namespace Core.Interfaces
 {
+    // Os método add, update e delete não são async porque não iremos diretamente adicioná-los no banco.
+    // Estas operações são feitas em memória.
+    // O repositório não salva dados, essa tarefa é do UnitOfWork
     public interface IGenericRepository<T> where T : BaseEntity
     {
         Task<T> GetByIdAsync(int id);
@@ -12,5 +15,8 @@ namespace Core.Interfaces
         Task<T> GetEntityWithSpec(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
         Task<int> CountAsync(ISpecification<T> spec);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
